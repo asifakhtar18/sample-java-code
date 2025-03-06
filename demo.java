c561134b813c5830431c151dedca22b36bf9d88a
Md Asif Akhtar <109073948+asifakhtar18@users.noreply.github.com>
2025-03-06 20:08:40 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,8 +1,7 @@
 public class demo {
     public static void main(String[] args) {
         // Print a message to the console as output
-        System.out.println("Hello welcome to jenkins");
-        
+        System.out.println("Hello welcome to jenkins");     
     } 
 }  
 
